47945453339bb91c58a97aecbff2f0d5c39612ab
maycon164 <mayconfelipe164@gmail.com>
2021-09-28 11:40:45 -0300

    making events with btn's

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -3,7 +3,61 @@ class WhatsAppController {
     constructor() {
         this.elementsPrototype();
         this.initElements();
+        this.initEvents();
     }
+
+    initEvents() {
+
+        this.el.myPhoto.on('click dblclick', e => {
+            this.closeAllPanelLeft();
+            this.el.panelEditProfile.show().addClass('open');
+
+        })
+
+        this.el.btnNewContact.on('click', e => {
+            this.closeAllPanelLeft();
+            this.el.panelAddContact.show().addClass('open');
+
+        });
+
+        this.el.btnClosePanelEditProfile.on('click', e => {
+            this.el.panelEditProfile.removeClass('open');
+        });
+
+        this.el.btnClosePanelAddContact.on('click', e => {
+            this.el.panelAddContact.removeClass('open');
+        })
+
+        this.el.imgDefaultPanelEditProfile.on('click', e => {
+            this.el.inputProfilePhoto.click();
+        });
+
+        this.el.inputNamePanelEditProfile.on('keypress', e => {
+
+            if (e.key == 'Enter') {
+                e.preventDefault();
+                this.el.btnSavePanelEditProfile.click();
+            }
+
+        });
+
+        this.el.btnSavePanelEditProfile.on('click', e => {
+            console.log(this.el.inputNamePanelEditProfile.innerHTML);
+
+        });
+
+        this.el.formPanelAddContact.on('submit', e => {
+            e.preventDefault();
+
+        });
+
+    }
+
+    closeAllPanelLeft() {
+        this.el.panelEditProfile.hide();
+        this.el.panelAddContact.hide();
+    }
+
 
     initElements() {
         this.el = {};
@@ -12,57 +66,71 @@ class WhatsAppController {
             this.el[Format.getCamelCase(element.id)] = element;
         });
 
-   }
+    }
 
-   elementsPrototype(){
+    elementsPrototype() {
 
-        Element.prototype.hide = function(){
+        Element.prototype.hide = function () {
             this.style.display = 'none';
             return this;
         }
 
-        Element.prototype.show = function(){
+        Element.prototype.show = function () {
             this.style.display = 'block';
             return this;
         }
 
-        Element.prototype.toggle = function(){
-            this.style.display =  (this.style.display == 'none') ? 'block':'none';
+        Element.prototype.toggle = function () {
+            this.style.display = (this.style.display == 'none') ? 'block' : 'none';
             return this;
         }
 
-        Element.prototype.on = function(events, fn){
-             events.split(" ").forEach(event => {
+        Element.prototype.on = function (events, fn) {
+            events.split(" ").forEach(event => {
                 this.addEventListener(event, fn);
-             });
-             return this;
+            });
+            return this;
         }
 
-        Element.prototype.css = function(styles){
-            for(let name in styles){
+        Element.prototype.css = function (styles) {
+            for (let name in styles) {
                 this.style[name] = styles[name];
             }
             return this;
         }
 
-        Element.prototype.addClass = function(className){
+        Element.prototype.addClass = function (className) {
             this.classList.add(className);
             return this;
         }
 
-        Element.prototype.removeClass = function(className){
+        Element.prototype.removeClass = function (className) {
             this.classList.remove(className);
             return this;
         }
 
-        Element.prototype.toggleClass = function(className){
+        Element.prototype.toggleClass = function (className) {
             this.classList.toggle(className);
             return this;
         }
 
-        Element.prototype.hasClass = function(className){
+        Element.prototype.hasClass = function (className) {
             return this.classList.contains(className);
         }
-   }
+
+        HTMLFormElement.prototype.getForm = function () {
+            return new FormData(this);
+        }
+
+        HTMLFormElement.prototype.toJSON = function () {
+            let json = {};
+
+            this.getForm().forEach((key, value) => {
+                json[key] = value;
+            });
+
+            return json;
+        }
+    }
 
 }
