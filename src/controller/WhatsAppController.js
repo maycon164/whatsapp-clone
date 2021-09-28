9f8257362c3521748a07f9190f5bc8f948fed919
maycon164 <mayconfelipe164@gmail.com>
2021-09-28 14:17:37 -0300

    main's panel and + btn events

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -51,11 +51,130 @@ class WhatsAppController {
 
         });
 
+        this.el.contactsMessagesList.querySelectorAll('.contact-item').forEach(item => {
+
+            item.addEventListener('click', e => {
+
+                this.el.home.hide();
+                this.el.main.css({ display: "flex" });
+
+            });
+
+        });
+
+        this.el.btnAttach.on('click', e => {
+            e.stopPropagation();
+
+            this.el.menuAttach.show().addClass('open');
+
+            document.addEventListener('click', this.closeMenuAttach.bind(this));
+
+        })
+
+        this.el.btnAttachPhoto.on('click', e => {
+            this.el.inputPhoto.click();
+        })
+
+        this.el.inputPhoto.on('change', e => {
+            [...this.el.inputPhoto.files].forEach(file => {
+                console.log(file);
+            })
+        });
+
+        this.el.btnAttachCamera.on('click', e => {
+            this.closeAllMainPanel();
+
+            this.el.panelCamera.addClass('open').css({
+                height: "100%"
+            })
+
+        })
+
+        this.el.btnClosePanelCamera.on('click', e => {
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+
+        });
+
+        this.el.btnTakePicture.on('click', e => {
+            console.log("TAKE A PICTURE");
+        });
+
+        this.el.btnAttachDocument.on('click', e => {
+            this.closeAllMainPanel();
+
+            this.el.panelDocumentPreview.addClass('open').css({
+                height: "100%"
+            })
+
+        });
+
+        this.el.btnClosePanelDocumentPreview.on('click', e => {
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+
+        });
+
+        this.el.btnSendDocument.on('click', e => {
+            console.log('send document ..... ');
+        });
+
+        this.el.btnAttachContact.on('click', e => {
+            this.el.modalContacts.show();
+        })
+
+        this.el.btnCloseModalContacts.on('click', e => {
+            this.el.modalContacts.hide();
+        });
+
+        this.el.btnSendMicrophone.on('click', e => {
+            this.el.recordMicrophone.show();
+            this.el.btnSendMicrophone.hide();
+            this.startRecordMicrophoneTime();
+        });
+
+        this.el.btnFinishMicrophone.on('click', e => {
+            this.closeRecordMicrophone();
+        });
+
+        this.el.btnCancelMicrophone.on('click', e => {
+            this.closeRecordMicrophone();
+        })
+
     }
+
+    startRecordMicrophoneTime() {
+        let start = Date.now();
+
+        this._microphoneTimer = setInterval(() => {
+            this.el.recordMicrophoneTimer.innerHTML = Format.toTime((Date.now() - start));
+        }, 100);
+
+    }
+
+    closeRecordMicrophone() {
+        this.el.recordMicrophone.hide();
+        this.el.btnSendMicrophone.show();
+        clearInterval(this._microphoneTimer);
+    }
+
+    closeAllMainPanel() {
+        this.el.panelMessagesContainer.hide();
+        this.el.panelCamera.removeClass('open');
+        this.el.panelDocumentPreview.removeClass('open');
+    }
+
 
     closeAllPanelLeft() {
         this.el.panelEditProfile.hide();
         this.el.panelAddContact.hide();
+    }
+
+    closeMenuAttach(e) {
+
+        this.el.menuAttach.removeClass('open');
+        document.removeEventListener('click', this.closeMenuAttach);
+
     }
 
 
--- src/utils/Format.js
+++ src/utils/Format.js
@@ -1,11 +1,17 @@
-class Format{
+class Format {
 
-    static getCamelCase(txt){
+    static getCamelCase(txt) {
         let div = document.createElement("div");
         div.innerHTML = `<div data-${txt} = "id"> </div>`;
 
         return Object.keys(div.firstChild.dataset)[0];
-        
+
     }
 
+    static toTime(time) {
+        let seconds = parseInt((time / 1000) % 60);
+        let minutes = parseInt((time / (1000 * 60)) % 60);
+
+        return `${minutes}:${seconds.toString().padStart(2, 0)}`;
+    }
 }
