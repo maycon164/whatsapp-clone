17136f02a63f7eb690ff92b81a0ca44438c51286
maycon164 <mayconfelipe164@gmail.com>
2021-09-28 10:47:20 -0300

    functions at prototype Element

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,6 +1,7 @@
 class WhatsAppController {
 
     constructor() {
+        this.elementsPrototype();
         this.initElements();
     }
 
@@ -11,7 +12,57 @@ class WhatsAppController {
             this.el[Format.getCamelCase(element.id)] = element;
         });
 
-        
+   }
 
-    }
+   elementsPrototype(){
+
+        Element.prototype.hide = function(){
+            this.style.display = 'none';
+            return this;
+        }
+
+        Element.prototype.show = function(){
+            this.style.display = 'block';
+            return this;
+        }
+
+        Element.prototype.toggle = function(){
+            this.style.display =  (this.style.display == 'none') ? 'block':'none';
+            return this;
+        }
+
+        Element.prototype.on = function(events, fn){
+             events.split(" ").forEach(event => {
+                this.addEventListener(event, fn);
+             });
+             return this;
+        }
+
+        Element.prototype.css = function(styles){
+            for(let name in styles){
+                this.style[name] = styles[name];
+            }
+            return this;
+        }
+
+        Element.prototype.addClass = function(className){
+            this.classList.add(className);
+            return this;
+        }
+
+        Element.prototype.removeClass = function(className){
+            this.classList.remove(className);
+            return this;
+        }
+
+        Element.prototype.toggleClass = function(className){
+            this.classList.toggle(className);
+            return this;
+        }
+
+        Element.prototype.hasClass = function(className){
+            return this.classList.contains(className);
+        }
+   }
+
 }
