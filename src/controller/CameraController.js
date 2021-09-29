2e4f682b8df186b1570c6f33b33fbbbb2faca39c
maycon164 <mayconfelipe164@gmail.com>
2021-09-28 21:25:57 -0300

    getting camera device

---
+++ src/controller/CameraController.js
@@ -0,0 +1,13 @@
+class CameraController {
+    constructor(videoEl) {
+        this.videoEl = videoEl;
+
+        navigator.mediaDevices.getUserMedia({
+            video: true
+        }).then(stream => {
+            this.videoEl.srcObject = stream;
+            this.videoEl.play();
+        }).catch(e => console.log(e));
+
+    }
+}
